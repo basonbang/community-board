4f4926c4a85181bfe33d9c7d4e3708606a76197b
Jason <jasontang20043@gmail.com>
2024-02-25 19:39:23 -0800

    Create header component and styles

---
+++ src/components/GymList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GymList = () => {
+  return ( 
+    <>
+      <main>
+        
+      </main>
+    </>
+   );
+}
+ 
+export default GymList;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="\3060350-25_10-removebg-preview.png" alt="Header image" />
+      <h1> Strength Training Suggestions </h1>
+    </header>
+  )
+}
+
+export default Header;
